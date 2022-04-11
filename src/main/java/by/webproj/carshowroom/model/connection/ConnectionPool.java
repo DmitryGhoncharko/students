0a75e6e91f2d937c1616d2c224933cfea4994938
Dmitry Ghoncharko <dafae722@gmail.com>
2022-04-11 17:23:37 +0300

    create connection pool with hikari cp implementation

---
+++ src/main/java/by/webproj/carshowroom/model/connection/ConnectionPool.java
@@ -0,0 +1,9 @@
+package by.webproj.carshowroom.model.connection;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public interface ConnectionPool {
+
+    Connection getConnection() throws SQLException;
+}
